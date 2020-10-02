dc7aa0033a79e64b7a1b5c17adcc45ac602ba9c9
Xavier Dufour-Maheux <xavierdufourmaheux@gmail.com>
2020-10-02 16:28:10 -0400

    Initial commit (Ugly code)

---
+++ src/xavierdufour/Main.java
@@ -0,0 +1,11 @@
+package xavierdufour;
+
+import xavierdufour.engine.GameWindow;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GameWindow window = new GameWindow();
+        window.start();
+    }
+}
